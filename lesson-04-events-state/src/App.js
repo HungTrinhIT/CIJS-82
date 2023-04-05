c49ed7cea5301c62e6bc9a2192cd48e58feec7ad
HungTrinhJH <hung.trinh@journeyh.io>
2023-04-05 20:58:03 +0700

    add: complete lesson 05

--- lesson-04-events-state/src/App.js
+++ lesson-04-events-state/src/App.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
 import "./App.css";
+import TodoApp from "./TodoApp";
 
 function App() {
+  // let total = 10;
+
+  // useState là một function, và function nhận vào 1 initialValue
+  // useState là một hook (react hook)
+
+  // const totalState = useState(10);
+  // const totalValue = totalState[0];
+  // const totalFunction = totalState[1];
+
+  const [total, setTotal] = useState(10);
+
   const onAddToCart = () => {
-    console.log("Bạn vừa thêm một món hàng vào giỏ hàng thành công");
+    // console.log("Bạn vừa thêm một món hàng vào giỏ hàng thành công");
+    // console.log("Before adding to cart:", total);
+    // total += 1;
+    // console.log("After adding to cart:", total);
+
+    // const newTotal = totalValue + 1;
+    // totalFunction(newTotal);
+
+    setTotal(total + 1);
+  };
+
+  const onRemoveOneItem = () => {
+    if (total === 0) return;
+
+    setTotal(total - 1);
   };
 
   const onDeleteItem = () => {
@@ -43,17 +70,21 @@ function App() {
 
   return (
     <div className="App">
+      <div className="cart">
+        <p>Cart: ({total})</p>
+      </div>
       <div
         className="product"
         style={{
           border: "1px solid black",
           padding: "24px",
-          width: "400px",
+          width: "300px",
+          marginBottom: "24px",
         }}
       >
         <h6>IPhone 14 Promax</h6>
-        <button onClick={onAddToCart}>Add to cart</button>
-        <button onClick={onDeleteItem}>Delete item from cart</button>
+        <button onClick={onAddToCart}>(+) Add to cart</button>
+        <button onClick={onRemoveOneItem}>(-) Remove one item</button>
       </div>
 
       {/* Cach 1 */}
@@ -63,9 +94,28 @@ function App() {
       {/* Cach 2 */}
       {/* <button onClick={onSortCart("asc")}>Sap xep tang dan</button>
       <button onClick={onSortCart("des")}>Sap xep giam dan</button> */}
+      <TodoApp />
     </div>
   );
 }
 //  Lan dau render: functicn a()
 //  onSortCart("asc")()
 export default App;
+
+// Re-render
+// Datatypes: primitives & references
+
+/*
+
+  CRUD
+    C: Create (Add)
+    R: Read
+    U: Update
+    D: Delete
+
+    Shopping cart
+    => Add new product to cart
+    => Update quantity cart item
+    => Remove (delete) item to cart
+    => Read all items (render all item)
+*/
